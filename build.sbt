a3c2048407a58c97b7bd6ec5caeaaa806df011f3
Lukasz Dembinski <dembol@devesion.com>
2017-04-22 20:05:56 +0200

    ClusterRouterGroupFactory added

--- build.sbt
+++ build.sbt
@@ -5,12 +5,14 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= {
-  val akkaV = "2.4.16"
-  val akkaHttpV = "10.0.4"
+  val akkaV = "2.5.0"
+  val akkaHttpV = "10.0.5"
   val scalazVersion = "7.1.9"
   Seq(
     "org.scala-lang" % "scala-reflect" % scalaVersion.value,
     "com.typesafe.akka" %% "akka-actor" % akkaV,
+    "com.typesafe.akka" %% "akka-cluster" % akkaV,
+    "com.typesafe.akka" %% "akka-cluster-tools" % akkaV,
     "com.typesafe.akka" % "akka-http-spray-json-experimental_2.11" % "2.4.11",
 
     "org.scalacheck" % "scalacheck_2.11" % "1.13.5" % "test",
---
+++ src/main/scala/io/scalaberries/akka/ClusterRouterGroupFactory.scala
@@ -0,0 +1,30 @@
+package io.scalaberries.akka
+
+import akka.actor.{ActorContext, ActorRef}
+import akka.cluster.routing.{ClusterRouterGroup, ClusterRouterGroupSettings}
+import akka.routing.{ConsistentHashingGroup, RandomGroup}
+import akka.routing.ConsistentHashingRouter.ConsistentHashMapping
+
+case class ClusterRouterGroupSpecification(name: String, path: String, role: String) {
+  def routerSettings(implicit context: ActorContext): ClusterRouterGroupSettings = {
+    ClusterRouterGroupSettings(totalInstances = 100, routeesPaths = List(path), allowLocalRoutees = true, useRole = Some(role))
+  }
+}
+
+object ClusterRouterGroupFactory {
+  def consistentHashingGroup(specification: ClusterRouterGroupSpecification)(hashMapping : ConsistentHashMapping)(implicit context: ActorContext): ActorRef = {
+    context.actorOf(
+      ClusterRouterGroup(
+        ConsistentHashingGroup(Nil, hashMapping = hashMapping),
+        specification.routerSettings
+      ).props(), name = specification.name)
+  }
+
+  def randomGroup(specification: ClusterRouterGroupSpecification)(implicit context: ActorContext): ActorRef = {
+    context.actorOf(
+      ClusterRouterGroup(
+        RandomGroup(Nil),
+        specification.routerSettings
+      ).props(), name = "credentialCheckRouter")
+  }
+}
