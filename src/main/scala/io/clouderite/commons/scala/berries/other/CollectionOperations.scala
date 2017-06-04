b185cfb98a74da0a179fe54b95faa78dbba69359
Lukasz Dembinski <dembol@devesion.com>
2017-06-04 23:30:20 +0200

    CollectionOperation added

---
+++ src/main/scala/io/clouderite/commons/scala/berries/other/CollectionOperations.scala
@@ -0,0 +1,6 @@
+package io.clouderite.commons.scala.berries.other
+
+object CollectionOperations {
+  implicit def toList[B, A <% B](l: List[A]): List[B] = l map { x ⇒ x: B }
+  implicit def toSet[B, A <% B](l: Set[A]): Set[B] = l map { x ⇒ x: B }
+}
