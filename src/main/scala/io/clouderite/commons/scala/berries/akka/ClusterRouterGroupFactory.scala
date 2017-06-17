573b3ff2986b2d0d63aece0e2303a2b6d6834794
Lukasz Dembinski <dembol@devesion.com>
2017-06-17 17:37:33 +0200

    added TailChoppingGroup cluster router

--- src/main/scala/io/clouderite/commons/scala/berries/akka/ClusterRouterGroupFactory.scala
+++ src/main/scala/io/clouderite/commons/scala/berries/akka/ClusterRouterGroupFactory.scala
@@ -3,11 +3,19 @@ package io.clouderite.commons.scala.berries.akka
 import akka.actor.{ActorContext, ActorRef}
 import akka.cluster.routing.{ClusterRouterGroup, ClusterRouterGroupSettings}
 import akka.routing.ConsistentHashingRouter.ConsistentHashMapping
-import akka.routing.{ConsistentHashingGroup, RandomGroup}
+import akka.routing.{ConsistentHashingGroup, Group, RandomGroup, TailChoppingGroup}
+
+import scala.concurrent.duration.FiniteDuration
 
 case class ClusterRouterGroupSpecification(name: String, path: String, role: String) {
   def routerSettings(implicit context: ActorContext): ClusterRouterGroupSettings = {
     ClusterRouterGroupSettings(totalInstances = 100, routeesPaths = List(path), allowLocalRoutees = true, useRole = Some(role))
+  }
+}
+
+case class TailChoppingSpecification(within: FiniteDuration, interval: FiniteDuration) {
+  def routerStrategy(implicit context: ActorContext): Group = {
+    TailChoppingGroup(Nil, within = within, interval = interval)
   }
 }
 
@@ -26,5 +34,12 @@ object ClusterRouterGroupFactory {
         RandomGroup(Nil),
         specification.routerSettings
       ).props(), name = specification.name)
+  }
+
+  def tailChoppingGroup(specification: ClusterRouterGroupSpecification, choppingSpecification: TailChoppingSpecification)(implicit context: ActorContext): ActorRef = {
+    context.actorOf(ClusterRouterGroup(
+      choppingSpecification.routerStrategy,
+      specification.routerSettings)
+      .props(), name = specification.name)
   }
 }
